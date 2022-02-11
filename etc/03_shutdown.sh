a669db53c96a2a5467fd0814934f31ca55870cfd
‘sghaha’ <ec2-user@ip-172-31-29-180.ap-northeast-2.compute.internal>
2022-02-11 08:23:21 +0000

    to move japan

--- etc/03_shutdown.sh
+++ etc/03_shutdown.sh
@@ -11,13 +11,13 @@ kubectl delete -f alb-ingress-controller/v2_2_1_full.yaml
 
 #kubectl delete -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-eksctl delete nodegroup --cluster=eks-demo --name=node-group
-eksctl delete cluster --name=eks-demo
+eksctl delete nodegroup --cluster=sghaha-eks --name=node-group
+eksctl delete cluster --name=sghaha-eks
 
 
 
 aws logs describe-log-groups --query 'logGroups[*].logGroupName' --output table | \
-awk '{print $2}' | grep ^/aws/containerinsights/eks-demo | while read x; do  echo "deleting $x" ; aws logs delete-log-group --log-group-name $x; done
+awk '{print $2}' | grep ^/aws/containerinsights/sghaha-eks | while read x; do  echo "deleting $x" ; aws logs delete-log-group --log-group-name $x; done
 
 aws logs describe-log-groups --query 'logGroups[*].logGroupName' --output table | \
-awk '{print $2}' | grep ^/aws/eks/eks-demo | while read x; do  echo "deleting $x" ; aws logs delete-log-group --log-group-name $x; done
+awk '{print $2}' | grep ^/aws/eks/sghaha-eks | while read x; do  echo "deleting $x" ; aws logs delete-log-group --log-group-name $x; done
